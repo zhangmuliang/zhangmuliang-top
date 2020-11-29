ed4578af666805e803006517ff499cd6e4d65b11
zhangmuliang <714978916@qq.com>
2020-11-29 08:42:04 +0800

    项目初始状态

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+// 根组件
+class App extends Component {
+    render() {
+        return (
+            <div>App</div>
+        )
+    }
+} 
+
+export default App
